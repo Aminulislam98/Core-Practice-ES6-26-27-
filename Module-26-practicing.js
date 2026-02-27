63e920422fbb495dee1de390d4ed84b7ea92ba56
Aminul islam <aminurrohman98@gmail.com>
2026-02-27 21:21:56 +0000

    practiceed about variable

--- Module-26-practicing.js
+++ Module-26-practicing.js
@@ -1,2 +1,14 @@
 // 🔹 26-1: var, let, const
 // 📝 Exercise 1: Block Scope পরীক্ষা
+{
+  var practice = "Module";
+  let practice1 = "Module1";
+  const practice2 = "Module2";
+  //   let and const is blocked scoped once we declared inside {} we will not get access! if we declared in global scope then we get access even we get access form {} because global scope goes  inner scope!
+}
+console.log(practice);
+// console.log(practice1);
+// console.log(practice2);
+
+// ==============================
+// 📝 Exercise 2: const object behaviour
